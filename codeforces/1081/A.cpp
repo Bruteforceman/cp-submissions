493faf2fa3ec615c9d838d5d5a0231e6060703fe
Bruteforceman <tasmeemreza@gmail.com>
2018-12-16 22:47:00 +0600

    Add solution for problem `A - Definite Game` Link: https://codeforces.com/contest/1081/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1081/submission/47134155

---
+++ codeforces/1081/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if(x <= 2) {
+        cout << x << endl;
+    } else {
+        cout << 1 << endl;
+    }
+}
