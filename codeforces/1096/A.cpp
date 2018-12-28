14ef2cd9ceffac19939cf0f06e33219dab94f8fc
Bruteforceman <tasmeemreza@gmail.com>
2018-12-28 20:37:00 +0600

    Add solution for problem `A - Find Divisible` Link: https://codeforces.com/contest/1096/problem/A Tags: greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1096/submission/47625671

---
+++ codeforces/1096/A.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin >> t;
+	while(t--) {
+		long long x, y;
+		cin >> x >> y;
+		cout << x << " " << x+x << endl; 
+	}
+	return 0;
+}
