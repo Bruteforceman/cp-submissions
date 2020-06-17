e357e83a3fc320e4fbed14e871ac766e1e29d93d
Bruteforceman <tasmeemreza@gmail.com>
2020-06-18 04:09:00 +0600

    Add solution for problem `C - Prefix Product Sequence` Link: https://codeforces.com/contest/487/problem/C Tags: constructive algorithms, math, number theory, *2300 Ref: https://codeforces.com/contest/487/submission/84114223

--- codeforces/487/C.py
+++ codeforces/487/C.py
@@ -2,7 +2,12 @@ import math
 import sys
 input = sys.stdin.readline
 n = int(input())
-if math.factorial(n - 1) % n == n - 1:
+def isPrime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+if isPrime(n):
     print('YES')
     print('\n'.join(list(map(str, [1] + [i * pow(i - 1, n - 2, n) % n for i in range(2, n)] + [n]))[:n]))
 elif n == 4:
